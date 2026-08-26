1264ae82ef38615d2b5f8830a2e418e72505b36b
adnanhasanhodzic <adnan.hasanhodzic@gmail.com>
2026-08-26 13:16:38 +0200

    Delete src directory

--- src/data.ts
+++
@@ -1,9 +0,0 @@
-// Storage keys - NO DEMO / HARDCODED DATA
-export const STORAGE_KEY_CARS = 'moj_auto_user_cars';
-export const STORAGE_KEY_ACTIVE_CAR_ID = 'moj_auto_active_car_id';
-export const STORAGE_KEY_CAR = 'moj_auto_user_car'; // For legacy single car migration
-export const STORAGE_KEY_RECORDS = 'moj_auto_user_records';
-export const STORAGE_KEY_OBLIGATIONS = 'moj_auto_user_obligations';
-
-// Common fuel options
-export const FUEL_OPTIONS = ['Dizel', 'Benzin', 'Benzin + Plin (LPG)', 'Hibrid', 'Električni', 'Plug-in Hibrid'];
